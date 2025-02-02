d34dec82d8029a8edfb1af8073a2d4b877750f50
ilaotovka <ilaotovka@gmail.com>
2025-02-02 14:00:19 +0200

    Додаю gitignore та файл з калькулятором

---
+++ calculator/calculator/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введіть перше число:");
+        int num1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Введіть друге число:");
+        int num2 = Convert.ToInt32(Console.ReadLine());
+
+        int sum = num1 + num2;
+
+        Console.WriteLine($"Сума: {sum}");
+    }
+}
